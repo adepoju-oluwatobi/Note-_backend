45ef6b3446701d01a4af60137e0c803f34208fe6
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-12-27 19:37:27 +0100

    aded more routes

--- note_routes.py
+++ note_routes.py
@@ -31,3 +31,68 @@ def add_note():
         return jsonify({'message': 'Note added successfully'}), 200
 
     return jsonify({'error': 'Method not allowed'}), 405  # Handling other HTTP methods
+
+
+@note_bp.route('/notes/edit/<int:note_id>', methods=['GET', 'PUT'])
+def edit_note(note_id):
+    if request.method == 'PUT':
+        data = request.json  # Assuming JSON data from the React component
+
+        # Check if the required fields are present in the request
+        if 'title' not in data or 'content' not in data or 'user_id' not in data:
+            return jsonify({'error': 'Please provide title, content, and user_id'}), 400
+
+        user_id = data['user_id']  # Get the user ID from the request
+
+        # Get the existing note from the database using the note_id
+        note = Note.query.filter_by(id=note_id).first()
+
+        # Check if the note exists
+        if not note:
+            return jsonify({'error': 'Note not found'}), 404
+
+        # Update the note details
+        note.title = data['title']
+        note.content = data['content']
+        note.user_id = user_id  # Update the user ID if needed
+
+        # Commit changes to the database
+        db.session.commit()
+
+        return jsonify({'message': 'Note updated successfully'}), 200
+
+    elif request.method == 'GET':  # Handle GET request separately
+        note = Note.query.filter_by(id=note_id).first()
+
+        if note:
+            # Prepare the note data to be sent back as a response
+            note_data = {
+                'id': note.id,
+                'title': note.title,
+                'content': note.content,
+                'user_id': note.user_id
+                # Include more fields as needed
+            }
+            return jsonify({'note': note_data}), 200
+        else:
+            return jsonify({'error': 'Note not found'}), 404
+
+    return jsonify({'error': 'Method not allowed'}), 405  # Handling other HTTP methods
+
+
+@note_bp.route('/notes/delete/<int:note_id>', methods=['DELETE'])
+def delete_note(note_id):
+    if request.method == 'DELETE':
+        # Get the note by ID
+        note = Note.query.get(note_id)
+
+        if not note:
+            return jsonify({'error': 'Note not found'}), 404
+
+        # Delete the note from the database
+        db.session.delete(note)
+        db.session.commit()
+
+        return jsonify({'message': 'Note deleted successfully'}), 200
+
+    return jsonify({'error': 'Method not allowed'}), 405
